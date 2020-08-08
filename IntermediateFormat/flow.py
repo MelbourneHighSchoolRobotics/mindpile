58b05744315dd9c419b3ad04d959c60abccc39f0
Dalordish <me@huangrichard.com>
2020-08-08 17:20:39 +1000

    add a basic flow resolution algorithm

--- IntermediateFormat/flow.py
+++ IntermediateFormat/flow.py
@@ -1,4 +1,15 @@
-import .objects
+import IntermediateFormat.objects as objects
 from typing import List
-def flowResolver([]) -> []:
-    pass
+
+
+def flowResolver(blocks: List[objects.SequenceBlock]) -> List[objects.SequenceBlock]:
+    inWireToBlock = {}
+    for block in blocks:
+        inWireToBlock[block.inputWire] = block
+
+    sortedBlocks = []
+    currBlock = inWireToBlock[None]  # starting block has in=None
+    while currBlock.outputWire != None:
+        sortedBlocks.append(currBlock)
+        currBlock = inWireToBlock[currBlock.outputWire]
+    return sortedBlocks
