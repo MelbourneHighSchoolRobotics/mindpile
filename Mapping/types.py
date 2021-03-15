2f07177807b44b5a06b42499b612e172ad7015df
Angus Trau <me@angus.ws>
2021-03-16 07:52:29 +1100

    Correct comment

--- Mapping/types.py
+++ Mapping/types.py
@@ -14,7 +14,7 @@ class EV3Type(ABC):
 class InPort(EV3Type):
     @staticmethod
     def parse(input: str):
-        # Convert "1.A" -> "inA"
+        # Convert "1.4" -> "in4"
         # TODO: EV3 allows selecting ports from other paired bricks (specified by the number)
         return "in" + input[2]
 
