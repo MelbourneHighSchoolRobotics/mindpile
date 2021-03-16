bfe288acc62a1f4df1af798f722e95ffe9ad5874
Angus Trau <me@angus.ws>
2021-03-16 16:30:19 +1100

    Add basic while loop conditions

--- Mapping/control.py
+++ Mapping/control.py
@@ -1,15 +1,31 @@
-from .types import Local
 from .utils import MethodCall
 
-@MethodCall(target="LoopIndex.vix", LoopIndex=int, i=Local(0))
+@MethodCall(target="LoopIndex.vix", LoopIterationCount=int, LoopIndex=int)
 def loopIndex():
     return '''
-        LoopIndex = i
-        i += 1
+        LoopIndex = LoopIterationCount - 1
+    '''
+
+@MethodCall(target="StopAfterNumberIterations.vix", IterationsToRun=int, LoopIterationCount=int, Result=bool)
+def stopAfterNumberIterations():
+    return '''
+        Result = LoopIterationCount >= IterationsToRun
+    '''
+
+@MethodCall(target="StopIfTrue.vix", DoStop=bool, Result=bool)
+def stopIfTrue():
+    return '''
+        Result = DoStop
     '''
 
 @MethodCall(target="StopNever.vix", Result=bool)
 def stopNever():
     return '''
         Result = False
+    '''
+
+@MethodCall(target="TimeCompareLoop.vix", HowLong=float, Result=bool, LoopEnterTime=float)
+def timeCompareLoop():
+    return '''
+        Result = time.time() >= LoopEnterTime + HowLong
     '''
