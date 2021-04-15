39edbf67cfcb005f12546342990292858d7dfae7
Angus Trau <me@angus.ws>
2021-04-15 22:09:01 +1000

    Refactor to_python function into the module

--- mindpile/__init__.py
+++ mindpile/__init__.py
@@ -1,1 +1,33 @@
+import os
+import tempfile
+from zipfile import ZipFile
+import ast
+import xml.etree.ElementTree as ET
+import mindpile.Utility.utility as utility
+import mindpile.Parser as Parser
+from mindpile.Mapping.utils import generateSetupAST, startCodeGen
+
 __version__ = "0.0.1"
+
+def to_python(ev3_path):
+    # Unzip the .ev3 file into a temporary location. Extract the ev3p file.
+    with tempfile.TemporaryDirectory() as tmpdir:
+        with ZipFile(ev3_path, "r") as zipObj:
+            zipObj.extractall(tmpdir)
+        ev3p_path = os.path.join(tmpdir, "Program.ev3p")
+
+        tree = ET.parse(ev3p_path)
+        root = tree.getroot()
+
+        for elem in root.iter(utility.addNameSpace("BlockDiagram")):
+            root = elem
+
+        codeBlock = Parser.tagHandler.translateElementToIRForm(root)
+        codeBlock.sortInternalFlow()
+        startCodeGen()
+        tree = ast.parse('')
+        main = codeBlock.toAST(ctx={})
+        tree.body += generateSetupAST().body
+        tree.body += main
+        tree = ast.fix_missing_locations(tree)
+        return ast.unparse(tree)
--- mindpile/main.py
+++ mindpile/main.py
@@ -1,47 +1,18 @@
-import ast
-import xml.etree.ElementTree as ET
-import mindpile.Utility.utility as utility
-import mindpile.Parser as Parser
+import sys
 import argparse
-from mindpile.Mapping.utils import generateSetupAST, startCodeGen
+from mindpile import to_python
 
 parser = argparse.ArgumentParser(description="Transpiles Mindstorms to ev3dev2 python.")
 
 parser.add_argument("mindstorms_path", type=str, help="The path to your mindstorms program (file ending with .ev3)")
 parser.add_argument("--output_path", "-o", type=str, default="code.py", dest="output_path", help="The file we output python code to.")
 
-def to_python(ev3p_path):
-    tree = ET.parse(ev3p_path)
-    root = tree.getroot()
-
-    for elem in root.iter(utility.addNameSpace("BlockDiagram")):
-        root = elem
-
-    codeBlock = Parser.tagHandler.translateElementToIRForm(root)
-    codeBlock.sortInternalFlow()
-    startCodeGen()
-    tree = ast.parse('')
-    main = codeBlock.toAST(ctx={})
-    tree.body += generateSetupAST().body
-    tree.body += main
-    tree = ast.fix_missing_locations(tree)
-    return ast.unparse(tree)
-
 def main(passed_args=None):
-    import os, sys
-    import tempfile
-    from zipfile import ZipFile
     args = parser.parse_args(passed_args or sys.argv[1:])
     # Some preliminary checks
     if not args.mindstorms_path.endswith(".ev3"):
         raise ValueError("The mindstorms file to extract should have the `.ev3` file ending.")
-    # First -> Unzip the .ev3 file into a temporary location. Extract the ev3p file.
-    with tempfile.TemporaryDirectory() as tmpdir:
-        with ZipFile(args.mindstorms_path, "r") as zipObj:
-            zipObj.extractall(tmpdir)
-        # Second -> Generate the python code.
-        python_source = to_python(os.path.join(tmpdir, "Program.ev3p"))
-    # Third -> Save to the output_path.
+    python_source = to_python(args.mindstorms_path)
     with open(args.output_path, "w") as f:
         f.write(python_source)
     print(f"Program written to {args.output_path}.")
